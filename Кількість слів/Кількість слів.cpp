69c1997a3b4a4fae31141d82434045b4326a2aa7
Я <@WIN-0EO50QJFU18>
2022-02-08 18:53:04 +0200

    Добавьте файлы проекта.

---
+++ Кількість слів/Кількість слів.cpp
@@ -0,0 +1,23 @@
+﻿// Підрахувати кількість слів у реченні.
+#include <iostream>
+#include<string>
+#include<Windows.h>
+using namespace std;
+
+ int  main()
+{
+    setlocale(LC_ALL, "rus");
+    int c = 0;
+    cout << "Введіть текст: ";
+    string text;
+    getline(cin, text);
+    int size = text.size();
+    for (int i = 1; i <size; i++)
+    {
+        if ((text[i - 1] != ' ') && (text[i] == ' ' )) {
+            c++; 
+        }
+    }
+
+    cout << "Кількість слів у цьому тексті =  " << c++ << endl;
+}
